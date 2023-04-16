c83ae51d51de4df79292eb0cbe1b8fdaf0438bbb
asi-isa <ali.inceoglu@protonmail.com>
2023-04-16 16:42:07 +0200

    version 2.0.0

--- trav.go
+++ trav.go
@@ -6,37 +6,45 @@ import (
 	"sync"
 )
 
-type TraverseClbFunc func(path string, entry fs.DirEntry)
+type Trav[T any] struct {
+	wg sync.WaitGroup
+	ch chan T
+}
 
-func traverse(wg *sync.WaitGroup, path string, fn TraverseClbFunc) {
+// Traverse traverses concurrently the file tree rooted at root, calling fn for each file in the tree and onEnd after every file and directory has been traversed.
+func (t *Trav[T]) Traverse(root string, fn func(path string, entry fs.DirEntry) (T, bool), onEnd func()) <-chan T {
+
+	clb := func(path string, entry fs.DirEntry) {
+		defer t.wg.Done()
+
+		val, ok := fn(path, entry)
+
+		if ok {
+			t.ch <- val
+		}
+	}
+
+	go func() {
+		defer onEnd()
+		defer t.wg.Wait()
+
+		t.traverse(root, clb)
+	}()
+
+	return t.ch
+}
+
+func (t *Trav[T]) traverse(path string, fn func(path string, entry fs.DirEntry)) {
 	dirEntries, err := os.ReadDir(path)
 
 	if err == nil {
 		for _, entry := range dirEntries {
 			if entry.IsDir() {
-				traverse(wg, path+"/"+entry.Name(), fn)
+				t.traverse(path+"/"+entry.Name(), fn)
 			} else {
-				wg.Add(1)
+				t.wg.Add(1)
 				go fn(path, entry)
 			}
 		}
 	}
-}
-
-// Traverse traverses concurrently the file tree rooted at root, calling fn for each file in the tree and onEnd after every file and directory has been traversed.
-func Traverse(root string, fn TraverseClbFunc, onEnd func()) {
-	var wg sync.WaitGroup
-
-	clb := func(path string, entry fs.DirEntry) {
-		defer wg.Done()
-
-		fn(path, entry)
-	}
-
-	go func() {
-		defer onEnd()
-		defer wg.Wait()
-
-		traverse(&wg, root, clb)
-	}()
 }
